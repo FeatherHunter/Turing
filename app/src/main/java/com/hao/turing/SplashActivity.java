0bdea8c4c9442b4dbfeda4b31916f5e8d928a794
王辰浩 <975559549@qq.com>
2017-12-10 00:21:13 +0800

    Turing项目启动-171210

---
+++ app/src/main/java/com/hao/turing/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.hao.turing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
