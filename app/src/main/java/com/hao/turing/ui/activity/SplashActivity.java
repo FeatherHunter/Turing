bf659c763cbee15eb75fc240b51dab89f7a30829
suhao <2301230182@qq.com>
2017-12-19 20:12:51 +0800

    my develop branch first commit

--- app/src/main/java/com/hao/turing/ui/activity/SplashActivity.java
+++ app/src/main/java/com/hao/turing/ui/activity/SplashActivity.java
@@ -1,9 +1,11 @@
-package com.hao.turing;
+package com.hao.turing.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.hao.turing.R;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -14,6 +16,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
 
     }
 }
